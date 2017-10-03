ddb86be33cdf00cf2e84f2d6e5d82f2b0fbf7347
Franc[e]sco <lolisamurai@tfwno.gf>
2017-10-03 06:00:34 +0200

    vcvarsall: fix copypasta indenting

--- vcvarsall17.ps1
+++ vcvarsall17.ps1
@@ -7,9 +7,9 @@ param (
 Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools"
 cmd /c "VsDevCmd.bat -arch=$arch&set" |
 ForEach-Object {
-  if ($_ -match "=") {
-    $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
-  }
+    if ($_ -match "=") {
+        $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
+    }
 }
 Pop-Location
 Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
