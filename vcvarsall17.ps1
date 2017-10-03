0f51798c2a39571171a2fc606039655b0fbcc83f
Franc[e]sco <lolisamurai@tfwno.gf>
2017-10-03 05:35:39 +0200

    vcvarsall17: don't use unix-style aliases

--- vcvarsall17.ps1
+++ vcvarsall17.ps1
@@ -4,12 +4,12 @@ param (
     [string]$arch = "amd64"
 )
 
-pushd "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools"
+Push-Location "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\Common7\Tools"
 cmd /c "VsDevCmd.bat -arch=$arch&set" |
-foreach {
+ForEach-Object {
   if ($_ -match "=") {
     $v = $_.split("="); set-item -force -path "ENV:\$($v[0])"  -value "$($v[1])"
   }
 }
-popd
+Pop-Location
 Write-Host "`nVisual Studio 2017 Command Prompt variables set." -ForegroundColor Yellow
