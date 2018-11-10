928aff7a36dadaaabc4932908aab6f2d79b8b158
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 00:48:39 +0100

    libbuild should build oppai.c, not main.c, also try -MD on windows

--- libbuild.bat
+++ libbuild.bat
@@ -4,7 +4,7 @@ del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
   -DNOMINMAX=1 ^
-  -O2 -nologo -LD -MT -Gm- -GR- -EHsc -W4 ^
-  main.c ^
+  -O2 -nologo -LD -MD -Gm- -GR- -EHsc -W4 ^
+  oppai.c ^
   -Feoppai.dll ^
   || EXIT /B 1
