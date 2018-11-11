d85e014f04d9976916356794fe8272f53287bda0
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 17:03:36 +0100

    build dll in 2 steps by calling link manually

--- libbuild.bat
+++ libbuild.bat
@@ -3,10 +3,11 @@
 del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 del oppai.exp >nul 2>&1
-cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
+cl -c -D_CRT_SECURE_NO_WARNINGS=1 ^
   -DNOMINMAX=1 ^
   -O2 -nologo -LD -MT -Gm- -GR- -EHsc -W4 ^
   -DOPPAI_EXPORT ^
   -D_WINDLL -D_USRDLL ^
   oppai.c ^
   || EXIT /B 1
+link -nologo -DLL oppai.obj || EXIT /B 1
