11c67301ebb6e7270dca161f898c4b39470a93f0
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 17:35:18 +0100

    2-step dll linking without -LD in the cl step

--- libbuild.bat
+++ libbuild.bat
@@ -3,10 +3,12 @@
 del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 del oppai.exp >nul 2>&1
+echo compiling
 cl ^
-  /O2 /nologo /Gm- /GR- /EHsc /W4 ^
+  /c /O2 /nologo /Gm- /GR- /EHsc /W4 ^
   /D_CRT_SECURE_NO_WARNINGS=1 /DNOMINMAX=1 ^
   /DOPPAI_EXPORT /D_WINDLL /D_USRDLL ^
   oppai.c ^
-  /LD ^
   || EXIT /B 1
+echo making dll
+link /NOLOGO /DLL oppai.obj
