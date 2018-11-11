bb86daff8aff299e98312d2254732e7befd5d80b
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 17:26:53 +0100

    libbuild: nologo is lowercase

--- libbuild.bat
+++ libbuild.bat
@@ -4,7 +4,7 @@ del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 del oppai.exp >nul 2>&1
 cl ^
-  /O2 /NOLOGO /Gm- /GR- /EHsc /W4 ^
+  /O2 /nologo /Gm- /GR- /EHsc /W4 ^
   /D_CRT_SECURE_NO_WARNINGS=1 /DNOMINMAX=1 ^
   /DOPPAI_EXPORT /D_WINDLL /D_USRDLL ^
   oppai.c ^
