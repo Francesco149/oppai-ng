8ef88f6004d7470e525196bcbb69fad314c4d22b
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 17:24:13 +0100

    libbuild: try specifying parameters the m$ way

--- libbuild.bat
+++ libbuild.bat
@@ -3,11 +3,10 @@
 del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 del oppai.exp >nul 2>&1
-cl -c -D_CRT_SECURE_NO_WARNINGS=1 ^
-  -DNOMINMAX=1 ^
-  -O2 -nologo -LD -MT -Gm- -GR- -EHsc -W4 ^
-  -DOPPAI_EXPORT ^
-  -D_WINDLL -D_USRDLL ^
+cl ^
+  /O2 /NOLOGO /Gm- /GR- /EHsc /W4 ^
+  /D_CRT_SECURE_NO_WARNINGS=1 /DNOMINMAX=1 ^
+  /DOPPAI_EXPORT /D_WINDLL /D_USRDLL ^
   oppai.c ^
+  /LD ^
   || EXIT /B 1
-link -nologo -DLL oppai.obj || EXIT /B 1
