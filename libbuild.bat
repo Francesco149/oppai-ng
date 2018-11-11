5423d17746884b958c5165aafbd674eaed89c583
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 14:50:09 +0100

    lib build should define OPPAI_IMPLEMENTATION, also switch back to -MT on windows

--- libbuild.bat
+++ libbuild.bat
@@ -4,7 +4,8 @@ del oppai.dll >nul 2>&1
 del oppai.obj >nul 2>&1
 cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
   -DNOMINMAX=1 ^
-  -O2 -nologo -LD -MD -Gm- -GR- -EHsc -W4 ^
+  -O2 -nologo -LD -MT -Gm- -GR- -EHsc -W4 ^
+  -DOPPAI_IMPLEMENTATION ^
   oppai.c ^
   -Feoppai.dll ^
   || EXIT /B 1
