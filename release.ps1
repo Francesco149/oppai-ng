69170ad74914389d1f7879b9308362b9d4105cb4
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 18:16:49 +0100

    unclutter release.ps1

--- release.ps1
+++ release.ps1
@@ -8,14 +8,21 @@ $dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Push-Location "$dir"
 git pull origin master -q
 
-Write-Host ""
-Write-Host "########################" -Foreground Yellow -Background Black
-Write-Host "> Compiling and stripping" -Foreground Yellow -Background Black
+function Write-Header {
+  param ([string]$Text)
+  Write-Host $Text -Foreground Yellow -Background Black
+}
+
+function Header {
+  param ([string]$Title)
+  Write-Header ""
+  Write-Header "##########################################################"
+  Write-Header "> $Title"
+}
+
 cmd /c "build.bat"
 cmd /c "libbuild.bat"
-Write-Host ""
-Write-Host "########################" -Foreground Yellow -Background Black
-Write-Host "> Packaging" -Foreground Yellow -Background Black
+Header "Packaging"
 $folder = "oppai-" + $(.\oppai.exe -version) + "-windows-"
 $clout = & cl 2>&1 | %{ "$_" }
 "$clout" -match "(Microsoft.*for )([a-z0-9\-_]+)" | Out-Null
@@ -36,9 +43,7 @@ if (Test-Path "$folder.zip") {
 &7z a "$folder.zip" $folder\oppai.exe $folder\oppai.dll $folder\oppai.lib `
   $folder\src
 
-Write-Host ""
-Write-Host "########################" -Foreground Yellow -Background Black
-Write-Host "> Result:" -Foreground Yellow -Background Black
+Header "Result:"
 &7z l "$folder.zip"
 
 Remove-Item $folder -Force -Recurse
