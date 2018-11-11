23e6bca82daf399bf772a40651cd6a34854e72c3
Franc[e]sco <lolisamurai@tfwno.gf>
2018-11-11 15:01:28 +0100

    define _USRDLL and _WINDLL on windows just in case it actually does something

--- libbuild.bat
+++ libbuild.bat
@@ -6,6 +6,7 @@ cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
   -DNOMINMAX=1 ^
   -O2 -nologo -LD -MT -Gm- -GR- -EHsc -W4 ^
   -DOPPAI_IMPLEMENTATION ^
+  -D_WINDLL -D_USRDLL ^
   oppai.c ^
   -Feoppai.dll ^
   || EXIT /B 1
