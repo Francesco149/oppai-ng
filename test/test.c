c1185002d4e5cff2aa8c322dbbea7bbf446a5179
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-09 17:22:41 +0100

    test: properly init parser when not using ezpp interface

--- test/test.c
+++ test/test.c
@@ -148,6 +148,7 @@ int main(int argc, char* argv[]) {
 trycalc:
 #endif
     f = fopen(fname_buf, "rb");
+    memset(pstate, 0, sizeof(parser_t));
     err = p_map(pstate, &map, f);
     if (err < 0) {
 #ifndef OPPAI_NOCURL
