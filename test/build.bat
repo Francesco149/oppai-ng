3a6e3dcfa40fc95c310361f122f732c65703a69e
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-19 03:18:52 +0100

    add script to build the test suite on windows

---
+++ test/build.bat
@@ -0,0 +1,10 @@
+@echo off
+
+del oppai_test.exe >nul 2>&1
+del test.obj >nul 2>&1
+cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
+  -DNOMINMAX=1 ^
+  -O2 -nologo -MT -Gm- -GR- -EHsc -W4 ^
+  test.c ^
+  -Feoppai_test.exe ^
+  || EXIT /B 1
