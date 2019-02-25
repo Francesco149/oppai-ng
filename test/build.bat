2bf7795dca19db1ae53d988c73589ada66d639f8
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-25 18:15:35 +0100

    attempt to fix windows test suite build

--- test/build.bat
+++ test/build.bat
@@ -1,10 +1,16 @@
 @echo off
 
+set flags="%*"
+IF "%1"=="" (
+  set flags=-DOPPAI_IMPLEMENTATION
+)
+
 del oppai_test.exe >nul 2>&1
 del test.obj >nul 2>&1
 cl  -D_CRT_SECURE_NO_WARNINGS=1 ^
   -DNOMINMAX=1 ^
   -O2 -nologo -MT -Gm- -GR- -EHsc -W4 ^
+  %flags% ^
   test.c ^
   -Feoppai_test.exe ^
   || EXIT /B 1
