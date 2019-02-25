53de9f1992518b332f0fea2c1699752f8f5582ed
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-25 16:23:18 +0100

    remove outdated unused windows docker script

--- build_containers.ps1
+++
@@ -1,7 +0,0 @@
-$dir = Split-Path -Parent $MyInvocation.MyCommand.Definition
-Push-Location "$dir"
-$d = "docker/windows"
-Copy-Item $d/Dockerfile .
-docker build -t "oppai-ng:windows" .
-Remove-Item Dockerfile
-Pop-Location
