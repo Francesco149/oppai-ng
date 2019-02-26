4c86ebabc2ac536bdae36537c8737c6f433996de
Franc[e]sco <lolisamurai@tfwno.gf>
2019-02-26 05:45:56 +0100

    add python mem example

---
+++ swig/python/examples/reuse_mem.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+
+import sys
+from oppai import *
+
+def mods_str(mods):
+  mods_str = "+"
+  if mods == 0:
+    mods_str += "nomod"
+  else:
+    if mods & MODS_HD: mods_str += "hd"
+    if mods & MODS_DT: mods_str += "dt"
+    if mods & MODS_HR: mods_str += "hr"
+  return mods_str
+
+ez = ezpp_new()
+ezpp_set_autocalc(ez, 1)
+for osufile in sys.argv[1:]:
+  # by providing the map in memory we can speed up subsequent re-parses
+  f = open(osufile, 'r')
+  data = f.read()
+  f.close()
+  ezpp_data(ez, data, len(data))
+  print("%s - %s [%s]" % (ezpp_artist(ez), ezpp_title(ez), ezpp_version(ez)))
+  print("%g stars" % ezpp_stars(ez))
+  for mods in [ 0, MODS_HR, MODS_HD | MODS_HR, MODS_DT, MODS_HD | MODS_DT ]:
+    print(mods_str(mods))
+    ezpp_set_mods(ez, mods)
+    for acc in range(95, 101):
+      ezpp_set_accuracy_percent(ez, acc)
+      print("%g%% -> %g pp" % (acc, ezpp_pp(ez)))
+  print("")
+ezpp_free(ez)
