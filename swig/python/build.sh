6db9bb4b08541d23d8691b1a08a6442f8ecf07ac
Franc[e]sco <lolisamurai@tfwno.gf>
2020-08-25 18:00:36 +0200

    python: switch to manylinux2010 as 1 isnt compatible with modern kerns

--- swig/python/build.sh
+++ swig/python/build.sh
@@ -4,7 +4,7 @@ rm -rf ./dist
 cp ../../oppai.c .
 cp ../oppai.i .
 swig -python -includeall oppai.i || exit
-for img in quay.io/pypa/manylinux1_x86_64 quay.io/pypa/manylinux1_i686; do
+for img in quay.io/pypa/manylinux2010_x86_64 quay.io/pypa/manylinux2010_i686; do
   docker run --user 1000:1000 --rm -v $(pwd):/io -w /io $img \
     ./build_wheels.sh || exit
 done
